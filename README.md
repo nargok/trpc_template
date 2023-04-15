3b92fb4854daf465a64ff38332be3a245a62fdce
nargok <nargok@gmail.com>
2023-04-15 16:51:04 +0900

    install fastify

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ TRPC_TEMPLATE
 
 ## npm workspace
 https://daveiscoding.com/nodejs-typescript-monorepo-via-npm-workspaces
+
+## install package
+
+```
+npm install -w packages/xxx <package_name>
+```
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Fuga from api")
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/", async (request, reply) => {
+    return { message: "hello from api" } 
+})
+
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listennig at ${address}`)
+})
